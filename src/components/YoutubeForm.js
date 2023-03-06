79d44270afa2d354a34f9e7d866367ea86bc1246
Thiago <tlcrezende@gmail.com>
2023-03-06 14:04:31 -0300

    add Formik, Form, Field and ErrorMessage

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -12,7 +12,7 @@ const onSubmit = (values) => {
   console.log(values);
 };
 
-// Yup é uma forte biblioteca para colocar regras de validações mais rápidas e mais fácies de manusear e ler 
+// Yup é uma forte biblioteca para colocar regras de validações mais rápidas e mais fácies de manusear e ler
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().email("email inválido").required("Required email"),
@@ -23,57 +23,43 @@ function YoutubeForm() {
   // useFormik é um hook que possui os objetos auxiliadores do Formik
   // initialValues são os valores inicias do forms, crucial aqui que o nome do initialValues coincida com o campo "name" dos forms de destino.
   // adicionando na tag form no onSubmit o handle Submit, toda vez que o forms for submitado ativara o onSubmit do formik e executara a arrow function predestinada.
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
+  // <Formik> component subistiu totalmente o useFormik hook
+  // const formik = useFormik({
+  //   initialValues,
+  //   onSubmit,
+  //   validationSchema,
+  // });
 
-    //formik.getFieldProps substitui os valores de formulário onChange={formik.handleChange}, onBlur={formik.handleBlur} e value={formik.values.nomeAqui} 
+  //formik.getFieldProps substitui os valores de formulário onChange={formik.handleChange}, onBlur={formik.handleBlur} e value={formik.values.nomeAqui}
+  // <Form> não precisa do onSubmit propr passando pra ele, já está diretamente ligado ao Formik
+  // <Field> já possui getFieldProps por debaixo dos panos e não precisa passar para ele "entender" essas funcionalidades
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            { ...formik.getFieldProps('name')}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            { ...formik.getFieldProps('email')}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email" />
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            { ...formik.getFieldProps('channel')}
-          />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" />
         </div>
 
         <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 }
 
